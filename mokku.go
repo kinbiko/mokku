3caeef273699dae27a83d328fba54cbe1213d5d5
Roger Guldbrandsen <roger@mercari.com>
2020-03-23 18:48:54 +0900

    [fix] Document exported function

--- mokku.go
+++ mokku.go
@@ -2,6 +2,8 @@ package mokku
 
 import "context"
 
+// Mock creates the sourcecode of a mock implementation of the interface
+// sourcecode defined in the given byte array.
 func Mock(ctx context.Context, data []byte) ([]byte, error) {
 	// TODO: hardcoded for now
 	return []byte(`
